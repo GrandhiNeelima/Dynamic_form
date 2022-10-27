688d6232fc2bb5e780ac87aaf9f90dc28f3c1c9e
neelimagrandhi1997 <neelima@crownfieldservices.net>
2022-10-27 15:37:15 +0530

    initial commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,24 +1,13 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
 import { JsonFormData } from '../components/dynamic-form/dynamic-form.component';
-//import { JsonFormData } from '../components/dynamic-form/dynamic-form.component';
-@Component({
+
+Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
-//public formData : FormData;
-  /*constructor(private http : HttpClient) {}
-  ngOninit(){
-    this.http
-      .get('/assets/dynamic-form.json')
-      .subscribe((formData: JsonFormData) => {
-        this.formData = formData;
-      });
-  }*/
-
   public formData: JsonFormData;
 
   constructor(private http: HttpClient) {}
